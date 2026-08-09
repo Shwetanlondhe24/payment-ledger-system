40988c9d1e7e56f1093e589ef71e57e539d49ffc
Shwetanlondhe24 <shwetan.college@gmail.com>
2026-08-09 19:23:59 +0530

    chore: initialize payment and ledger services

---
+++ payment-service/src/main/java/com/wallet/payment/entity/Transaction.java
@@ -0,0 +1,12 @@
+package com.wallet.payment.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigDecimal;
+
+
+public class Transaction {
+
+}
